9fd0ddfb8a1815e43b779e782be3c859a2a8e12e
Pooja Deshpande <pooja.is.deshpande@gmail.com>
2021-08-26 16:44:52 +0530

    FlyyFramework pod installed by declaring dependency in plugin.xml file

--- src/ios/FlyyPlugin.swift
+++ src/ios/FlyyPlugin.swift
@@ -1,1 +1,14 @@
+//import FlyyF
 
+@objc (FlyyPlugin) class FlyyPlugin: CDVPlugin {
+    
+    @objc(intSdk:) func initSdk(command: CDVInvokedUrlCommand) {
+        
+        let partnerToken = (command.arguments[0] as? NSObject)?.value(forKey: "partner_token") as? String
+        let environment = (command.arguments[1] as? NSObject)?.value(forKey: "environment") as? String
+        
+        
+        
+    }
+    
+}
